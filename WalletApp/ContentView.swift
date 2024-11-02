14350373fc2643f4e1c8386f023758cf848c7ff5
Goutham <gouthamdevaraju@gmail.com>
2024-11-02 12:20:47 +0530

    Added initial project

---
+++ WalletApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WalletApp
+//
+//  Created by Goutham Devaraju on 02/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WalletApp/WalletAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletAppApp.swift
+//  WalletApp
+//
+//  Created by Goutham Devaraju on 02/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WalletAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
